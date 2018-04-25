5b1ed95564f291b32e18ffc2c00198baad364bbd
SVT <samvantran@users.noreply.github.com>
2018-04-24 21:10:36 -0700

    Update build and add sdk clis (#6)
    Had trouble installing this out of the box. Ran into issues with go
    and dcos-filebeat and the unstable-v3 library. Looks like the
    build_framework script is also out of the date with sdk's new
    build_package.sh.
    
    I copied over the binaries that the sdk frameworks use.
    
    These changes should allow `./build.sh aws` to work OOTB.
    
    Note: this is the related error message I would get:
    
    ```
    Building dcos-service-cli-linux for linux in cli/dcos-filebeat/
    ../../../dcos-commons/cli/commands/config.go:36:126: cannot use cmd.handleConfigList (type func(*kingpin.Application, *kingpin.ParseElement, *kingpin.ParseContext) error) as type kingpin.Action in argument to config.Command("list", "(DEPRECATED, use 'debug config list') List IDs of all available configurations").Hidden().Action
    ../../../dcos-commons/cli/commands/config.go:38:127: cannot use cmd.handleConfigShow (type func(*kingpin.Application, *kingpin.ParseElement, *kingpin.ParseContext) error) as type kingpin.Action in argument to config.Command("show", "(DEPRECATED, use 'debug config show') Display a specified configuration").Hidden().Action
    ../../../dcos-commons/cli/commands/config.go:41:122: cannot use cmd.handleConfigTarget (
    ```

--- build.sh
+++ build.sh
@@ -6,9 +6,15 @@ BUILD_DIR=$FRAMEWORK_DIR/build/distributions
 
 FRAMEWORK_NAME=prometheus
 
+# Build/test our scheduler.zip
+${FRAMEWORK_DIR}/gradlew -p ${FRAMEWORK_DIR} check distZip
+
 DOCUMENTATION_PATH="https://github.com/mesosphere/dcos-commons-siumulation/blob/master/monitor/prometheus/README.md" \
     $FRAMEWORK_DIR/tools/build_framework.sh \
         $FRAMEWORK_NAME \
         $FRAMEWORK_DIR \
-        --artifact "$BUILD_DIR/${FRAMEWORK_NAME}-scheduler.zip" \
+        -a "$BUILD_DIR/${FRAMEWORK_NAME}-scheduler.zip" \
+        -a "$FRAMEWORK_DIR/sdk/cli/dcos-service-cli-linux" \
+    	-a "$FRAMEWORK_DIR/sdk/cli/dcos-service-cli-darwin" \
+    	-a "$FRAMEWORK_DIR/sdk/cli/dcos-service-cli.exe" \
         $@
